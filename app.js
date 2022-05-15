e2493d2a14332418c4d0934d08e704661841678b
RoniGraizman <ronigraiz@gmail.com>
2022-05-15 15:18:59 -0300

    css y html de  home.html

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
+
 const path = require('path')
+
 const app = express()
 
 app.use(express.static(path.join(__dirname,'public')))
@@ -7,7 +9,7 @@ app.use(express.static(path.join(__dirname,'public')))
 const PORT = 3000
 
 app.listen(PORT, () => {
-    console.log ('Corriendo en el puerto ' + PORT)
+    console.log ('corriendo en el puerto ' + PORT)
 })
 
 app.get('/', (req,res) =>{
