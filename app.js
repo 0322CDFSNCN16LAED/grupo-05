bae035ba75666c0acc882190770393dd0a6686ca
RoniGraizman <ronigraiz@gmail.com>
2022-05-17 13:10:51 -0300

    opciones de producto html y css

--- app.js
+++ app.js
@@ -16,3 +16,8 @@ app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname,'/views/home.html'))
 })
 
+app.get('/product-options', (req,res) =>{
+    res.sendFile(path.join(__dirname,'/views/product-options.html'))
+})
+
+
