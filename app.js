0c4a2d37773907efb1f0e94f2a5c612f27236d5d
Denise Ladowsky <dladowsky@gmail.com>
2022-05-23 10:17:28 -0300

    register y login

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-const express = require('express')
+const express = require("express");
 
-const path = require('path')
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = 3000
+const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log ('corriendo en el puerto ' + PORT)
-})
+  console.log("corriendo en el puerto " + PORT);
+});
 
-app.get('/', (req,res) =>{
-    res.sendFile(path.join(__dirname,'/views/home.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
 
-app.get('/product-options', (req,res) =>{
-    res.sendFile(path.join(__dirname,'/views/product-options.html'))
-})
+app.get("/product-options", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/product-options.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+});
 
 
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
+});
