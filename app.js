ff9cb0eb314190bcd5782de9a14feb6a53f7e5dd
RoniGraizman <ronigraiz@gmail.com>
2022-05-23 22:08:59 -0300

    header

--- app.js
+++ app.js
@@ -15,19 +15,15 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/home.html"));
 });
-
 app.get("/product-options", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/product-options.html"));
 });
-
 app.get("/product-detail", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/product-detail.html"));
 });
 app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/register.html"));
 });
-
-
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/login.html"));
 });
