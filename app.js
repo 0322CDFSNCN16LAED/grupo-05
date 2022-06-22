ee66b926bc6978c0654ffe83550bbe9ec65e0882
JavitoAlonso <jaalonsosilva@gmail.com>
2022-06-22 13:52:18 -0300

    modifico enlace ancor

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require ("express");
 const path = require("path");
 const app = express();
-// const methodOverride = require("method-override");
+const methodOverride = require("method-override");
 
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
-// app.use(methodOverride("_method"));
+app.use(methodOverride("_method"));
 
 const PORT= 3030;
 app.listen (PORT,()=>{
