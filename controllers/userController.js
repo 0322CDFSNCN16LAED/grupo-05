1d2e8050c72c0c0f0cdb619c06b8a46336dd690b
JavitoAlonso <jaalonsosilva@gmail.com>
2022-08-16 15:09:45 -0300

    modify y destroy

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const bcryptjs = require('bcryptjs');
 const { validationResult } = require('express-validator');
+const db = require('../database/models');
 const { User } = require("../database/models");
 const { Category } = require("../database/models");
 const { Service } = require("../database/models");
@@ -31,7 +32,7 @@ const controlador = {
                 photo = req.file.filename;       
             } */
         })
-        res.redirect("/")
+        res.redirect("/user/my-service");
     },
     myService: async (req,res) => {
        const services = await Service.findAll({
@@ -46,38 +47,61 @@ const controlador = {
         res.render ("my-service", { services })
     },
      deleteService:(req,res)=>{
-       const allprofessionals = dbProfessionals.getAll();
-       console.log(allprofessionals)
-       const filteredList = allprofessionals.filter((service)=>{
-            return service.id != req.params.id;
-       }) 
-       dbProfessionals.saveAll(filteredList);
+        Service.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+    //    const allprofessionals = dbProfessionals.getAll();
+    //    console.log(allprofessionals)
+    //    const filteredList = allprofessionals.filter((service)=>{
+    //         return service.id != req.params.id;
+    //    }) 
+    //    dbProfessionals.saveAll(filteredList);
       
-       res.redirect("/user/my-service");
+     res.redirect("/user/my-service");
     },
     modifyService:(req,res)=>{
-        let id = req.params.id;
-        res.render("modify-service", {serviceId : req.params.id})
+        let pedidoService = db.Service.findByPk(req.params.id)
+        let pedidoCategory = db.Category.findAll();
+        Promise.all([pedidoService,pedidoCategory])
+        .then(function([servicio,categoria]){
+            res.render("modify-service",{servicio,categoria})
+        })
+        // let id = req.params.id;
+        // res.render("modify-service", {serviceId : req.params.id})
     }, 
     serviceDetail: (req,res)=>{
-        const product = dbProfessionals.getOne(req.params.id);
-        res.render("service-detail",{product})
-        // res.render("service-detail",{product:product})
+        // const product = dbProfessionals.getOne(req.params.id);
+        // res.render("service-detail",{product})
+        // // res.render("service-detail",{product:product})
 
     },
     processModifyService:(req,res)=>{
-        const services = allProfessionals
-        const serviceIndex = services.findIndex((s)=> {s.id == req.params.id});
-        let service = services[serviceIndex];
+        Service.update ({
+            categoryId: req.body.profesion,
+            jobDescription: req.body.descripcion,
+            price: req.body.precio,
+            userId: req.session.userLogged.id
+        },{
+            where: {
+                id: req.params.id,
+            }
+        })
+        res.redirect("/user/my-service");
 
-        service.profesion = req.body.profesion; 
-        service.precio = req.body.precio;
-        service.descripcion = req.body.descripcion;
+        // const services = allProfessionals
+        // const serviceIndex = services.findIndex((s)=> {s.id == req.params.id});
+        // let service = services[serviceIndex];
 
-        dbProfessionals.saveAll(services)
+        // service.profesion = req.body.profesion; 
+        // service.precio = req.body.precio;
+        // service.descripcion = req.body.descripcion;
+
+        // dbProfessionals.saveAll(services)
     
         res.redirect('/user/my-service')
-        console.log(req.body)
+
     },
     logout: (req, res) => {
 		req.session.destroy();
