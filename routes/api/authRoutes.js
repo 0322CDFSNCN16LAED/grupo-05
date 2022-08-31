55e32a4c9d6fb1f846ec3edf8bc1c325d7c2a0b5
Javier Alonso <jaalonsosilva@gmail.com>
2022-08-31 13:12:18 -0300

    validation

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = express.Router();
+const authController = require("../../controllers/api/authController")
 
-
+routes.get("/email-validation", authController.list)
 
 module.exports = routes;
